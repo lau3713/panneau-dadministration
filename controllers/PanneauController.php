fa581486554af429d9f9811f3f2bdf9ecd3408a8
lau3713 <laurie.tis49@gmail.com>
2023-03-16 21:37:05 +0100

    first commit

--- controllers/PanneauController.php
+++ controllers/PanneauController.php
@@ -3,12 +3,11 @@ require_once "models/Panneau.class.php";
 class PanneauController
 {
     private $panneau;
-    // private $formulaire;
+
 
     public function __construct()
     {
         $this->panneau = new Panneau;
-        // $this->formulaire = new Restaurants;
     }
 
     public function afficherPanneau()
@@ -21,12 +20,6 @@ class PanneauController
     {
         require "views/formulaire.view.php";
     }
-
-    // public function modifierFormulaire($id)
-    // {
-    //     $pannea = $this->panneau->getProduitById($id);
-    //     require "views/formulaire2.view.php";
-    // }
 
     public function ajouterPanneau()
     {
@@ -43,18 +36,6 @@ class PanneauController
     public function modifierPanneau($id)
     {
 
-        // if (isset($_POST['id'])) {
-        //     $id = $_POST['id'];
-        //     $nom = $_POST['nom'];
-        //     $description = $_POST['description'];
-        //     $taille = $_POST['taille'];
-        //     $prix = $_POST['prix'];
-        //     $this->panneau->modifierProduit($nom, $description, $taille, $prix, $id);
-        //     header('Location: ' . "/php_optiondev/MVC/panneau-dadministration/");
-        // } else {
-        //     $produit = $this->panneau->getProduitById($id);
-        //     require "views/formulaire2.view.php";
-        // }
 
         if ($_GET['id']) {
             $produit = $this->panneau->getProduitById($id);
